ed53def39b75590598f3a9ccc1f0cdbafaf026c7
Egor Dorichev <egordorichev@gmail.com>
2017-05-22 11:50:55 +0300

    Add love-release config

--- src/conf.lua
+++ src/conf.lua
@@ -1,4 +1,18 @@
 function love.conf(t)
+	t.releases = {
+		title = "l2 engine",
+		package = "l2",
+		loveVersion = "0.10.2",
+		version = "0.1",
+		author = "Egor Dorichev",
+		email = "egordorichev@gmail.com",
+		description = "l2 engine template",
+		homepage = nil,
+		identifier = "l2",
+		excludeFileList = {},
+		releaseDirectory = nil
+	}
+    
     t.identity = "l2"
     t.version = "0.10.2"
     t.console = false
